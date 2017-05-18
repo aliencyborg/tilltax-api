5ba9b3d4c3c5fd74ab6c74bc126bd40d7a616391
Benjamin Rosas <ballPointPenguin@gmx.us>
2017-05-18 00:23:37 -0500

    add serializer

---
+++ lib/tilltax/guardian_serializer.ex
@@ -0,0 +1,12 @@
+defmodule TillTax.GuardianSerializer do
+  @behavior Guardian.Serializer
+
+  alias TillTax.Repo
+  alias TillTax.Accounts.User
+
+  def for_token(user = %User{}), do: { :ok, "User:#{user.id}" }
+  def for_token(_), do: { :error, "Unknown resource type" }
+
+  def from_token("User:" <> id), do: { :ok, Repo.get(User, id) }
+  def from_token(_), do: { :error, "Unknown resource type" }
+end
