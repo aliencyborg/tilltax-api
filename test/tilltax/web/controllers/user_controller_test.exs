6d0fbf54bdad2050ee0b3bf2a73c693cb1c261bd
Benjamin Rosas <ballPointPenguin@gmx.us>
2017-05-24 22:29:31 -0500

    user controller test

--- test/tilltax/web/controllers/user_controller_test.exs
+++ test/tilltax/web/controllers/user_controller_test.exs
@@ -1,25 +1,59 @@
 defmodule TillTax.Web.UserControllerTest do
   use TillTax.Web.ConnCase
 
-  alias TillTax.Accounts
+  alias TillTax.Accounts.User
+  alias TillTax.Repo
 
-  @create_attrs %{
-    "email" => "some@email",
-    "password" => "some-password",
-    "password-confirmation" => "some-password"
-  }
+  defp insert_user do
+    %User{
+      id: 123,
+      email: "admin@email",
+      password: Comeonin.Bcrypt.hashpwsalt("secretsauce")
+    } |> Repo.insert!
 
-  def fixture(:user) do
-    {:ok, user} = Accounts.create_user(@create_attrs)
-    user
+    Repo.get(User, 123)
   end
 
-  setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  setup %{conn: conn} = config do
+    cond do
+      config[:login] ->
+        user = insert_user()
+        signed_conn = Guardian.Plug.api_sign_in(conn, user)
+        {:ok, conn: put_req_header(signed_conn, "accept", "application/json")}
+      true ->
+        {:ok, conn: put_req_header(conn, "accept", "application/json")}
+    end
   end
 
+  @tag :login
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, user_path(conn, :index)
-    assert json_response(conn, 200)["data"] == []
+    assert json_response(conn, 200)["data"] == [%{
+      "attributes" => %{"email" => "admin@email"},
+      "id" => 123,
+      "type" => "user"
+    }]
+  end
+
+  test "does not list entries and renders errors when not logged in",
+  %{conn: conn} do
+    conn = get conn, user_path(conn, :index)
+    assert json_response(conn, 401)["errors"] != %{}
+  end
+
+  @tag :login
+  test "returns current user on current", %{conn: conn} do
+    conn = get conn, user_path(conn, :current)
+    assert json_response(conn, 200)["data"] == %{
+      "attributes" => %{"email" => "admin@email"},
+      "id" => 123,
+      "type" => "user"
+    }
+  end
+
+  test "does not return current usert and renders errors when not logged in",
+  %{conn: conn} do
+    conn = get conn, user_path(conn, :current)
+    assert json_response(conn, 401)["errors"] != %{}
   end
 end
